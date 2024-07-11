4490d318c595806a4977c8dd9095c7835411152d
Poul Poulsen <hello@poul.dev>
2024-07-11 13:35:43 +0200

    Extract Color Dropdown to its own component
    - ColorDropdown.tsx
    - Added Magenta

--- src/components/AddNewTimerForm.tsx
+++ src/components/AddNewTimerForm.tsx
@@ -1,6 +1,7 @@
 import { Action, ActionPanel, Color, Form, useNavigation } from "@raycast/api"
 import React, { useState } from "react"
 import { Timer } from "../utils/timerUtils"
+import { ColorDropdown } from "./ColorDropdown"
 
 interface AddNewTimerFormProps {
     onAdd: (timer: Timer) => void
@@ -33,14 +34,7 @@ export function AddNewTimerForm({ onAdd }: AddNewTimerFormProps) {
             }
         >
             <Form.TextField id="name" title="Name" value={name} onChange={setName} />
-            <Form.Dropdown id="color" title="Color" value={color} onChange={(newValue) => setColor(newValue as Color)}>
-                <Form.Dropdown.Item value={Color.Red} title="Red" />
-                <Form.Dropdown.Item value={Color.Green} title="Green" />
-                <Form.Dropdown.Item value={Color.Blue} title="Blue" />
-                <Form.Dropdown.Item value={Color.Yellow} title="Yellow" />
-                <Form.Dropdown.Item value={Color.Purple} title="Purple" />
-                <Form.Dropdown.Item value={Color.Orange} title="Orange" />
-            </Form.Dropdown>
+            <ColorDropdown value={color} onChange={(newValue) => setColor(newValue as Color)} />
         </Form>
     )
 }
